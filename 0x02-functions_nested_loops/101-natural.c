b29f6fff93ef7ea121af1b821817a9df947b3f29
Igraziella <igraziella2@gmail.com>
2022-09-14 12:21:12 -0700

    advanced

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main -computes and prints sum of all the multiples of 3 and 5 except 1024
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	total_sum = 0
+		for i in range(1, 1000);
+	if (i % 3 == 0 or i % 5 == 0)
+	total_sum = total_sum + i
+		print total_sum
+	       _putchar('\n');
+}
