12e4a3696535051cc7208e2e644b64dd6732a40d
Igraziella <igraziella2@gmail.com>
2022-09-13 18:10:54 -0700

    adding last digits

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int r;
+	int n;
 
 	print_last_digit(98);
 	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
+	n = print_last_digit(-1024);
+	_putchar('0' + n);
 	_putchar('\n');
 	return (0);
 }
