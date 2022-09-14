cead2aae7114a25a30bcfefd4a8e7b2d1dcbf36e
Igraziella <igraziella2@gmail.com>
2022-09-14 12:15:27 -0700

    updated table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table starting with 0.
+ *@n: number to be multiplied
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int n, i
+		;
+	printf("5");
+	scanf("%d", &5);
+	for (i = 1; i < 10; i++)
+	{
+		printf("%d * %d = %d \n", 5, i, 5 * i);
+	}
+	return (0);
+}
+
+
