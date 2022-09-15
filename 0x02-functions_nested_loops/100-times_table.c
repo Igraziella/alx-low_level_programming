2277474f9b084124b14a1785a925f1c1421c0868
Igraziellavc <igraziella2@gmail.com>
2022-09-15 13:17:03 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,25 +4,21 @@
 /**
  * print_times_table - Prints the n times table starting with 0.
  *@n: number to be multiplied
- * Return: Always 0.
+ * Return: nothing.
  */
 void print_times_table(int n)
 {
 	int i;
-	if (n > 15)
-	{
-		return;
-	}
-	if (n < 0)
-	{
-		return;
-	}
+
 	for (i = 0; i < n; i++)
 	{
-		_putchar(n * i);
-		_putchar('\n');
+		for (n * i)
+			if (n > 15 || n < 0)
+			{
+				_putchar(' ');
+				_putchar('\n');
+			}
 	}
-	return;
 }
 
 
