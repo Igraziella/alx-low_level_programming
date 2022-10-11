7db8ed145d94faacb103a009fec8df1a44c9eedf
Igraziellavc <igraziella2@gmail.comclear>
2022-10-11 15:35:36 -0700

    adding files

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+/**
+ * file - 3_function_like_macro
+ * desc: computes the absolute value of a number x
+ */
+
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+
+#endif
