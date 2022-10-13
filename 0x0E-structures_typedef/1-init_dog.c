ea8bfa95afb8327201252341c7b792f071581368
Igraziellavc <igraziella2@gmail.comclear>
2022-10-13 16:04:43 -0700

    minor changes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @age: age
  * @owner: owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return (NULL);
