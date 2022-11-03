ff3e5b789ddad27d0ff7d518946b29d697625238
Igraziella <igraziella2@gmail.com>
2022-11-03 03:23:23 -0700

    adding file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * read_textfile- Reads a text file and prints it
@@ -8,34 +9,34 @@
  * @letters: The number of letters the function would
  *		read and print.
  *
- * Return: 0, if funnction fails or filename is NULL
+ * Return: 0, if function fails or filename is NULL
  * otherwise: The actual number of bytes the function can read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	free(buffer);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(o);
-	return (w);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
